0a5bf572820b89d353a77826f384f413cc307921
Giuseppe Averta <g.averta3@gmail.com>
2024-05-14 19:16:12 +0200

    typo corrected

--- floyd.py
+++ floyd.py
@@ -7,7 +7,7 @@ G.add_weighted_edges_from([(0, 1, 5), (1, 2, 2),
 fw = nx.floyd_warshall(G, weight="weight")
 for a, b in fw.items():
     print(a)
-    print(b)
+    print(dict(b))
 
 results = {a: dict(b) for a, b in fw.items()}
 print(results)
